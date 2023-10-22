070234e5a28f823bbe4915fd761ad9280ce77a81
rasazim <ryanazimsk@gmail.com>
2023-10-22 09:46:29 +0530

    Changes to Node

--- A6 Startercode/Node.cpp
+++ A6 Startercode/Node.cpp
@@ -1,6 +1,13 @@
 #include "Node.h"
 
 Node::Node() {
+    book_code = 0;
+    page = 0;
+    paragraph = 0;
+    sentence_no = 0;
+    offset = 0;
+    left = NULL;
+    right = NULL;
 }
 
 Node::Node(int b_code, int pg, int para, int s_no, int off){
@@ -9,4 +16,6 @@ Node::Node(int b_code, int pg, int para, int s_no, int off){
     paragraph = para;
     sentence_no = s_no;
     offset = off;
+    left = NULL;
+    right = NULL;
 }
